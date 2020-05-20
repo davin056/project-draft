94463e8e6be003208c074fe48b6ae3271ddf7744
davin056 <65593334+davin056@users.noreply.github.com>
2020-05-20 22:28:04 +0700

    Update draft.cpp

--- draft.cpp
+++ draft.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <windows.h> // StdSetCursorPosition
 using namespace std;
 
 class Welcome {
@@ -16,9 +17,9 @@ class Welcome {
             operate.open("user.txt");
 
             cout << endl;
-            cout << "             -------------------------------------------------------------------------" << endl;
-            cout << "                                   Selamat datang di Kalkulator!                      " <<endl;
-            cout << "             -------------------------------------------------------------------------" << endl;
+            cout << "             --------------------------------------------------------------------------" << endl;
+            cout << "                                   Selamat datang di Kalkulator !                      " << endl;
+            cout << "             --------------------------------------------------------------------------" << endl;
             cout << "               Silahkan Masukkan Nama Anda : ";
             getline(cin, uname);
 
@@ -35,7 +36,7 @@ class Welcome {
 
         ~Welcome() {
             cout << "               Terima Kasih, " << uname << ", semoga harimu menyenangkan." << endl;
-
+            getch();
         }
 
 };
@@ -43,69 +44,97 @@ class Welcome {
 class Rumus : public Welcome{
     private :
         float bil1, bil2, hsl;
-        char oper;
+        string oper;
 
     public :
         Rumus(){
             operasi :
-                cout << "               Jenis Operasi ( + | - | x or * | : or / | ^ )? "; cin >> oper;
+                cout << "               Jenis Operasi ( + | - | x or * | : or / | ^ | sin | cos | tan )? "; getline(cin, oper);
 
-            if(oper == '+' | oper == '-' | oper == 'x' | oper == '*' | oper == ':' | oper == '^'){
-                input :
-                    cout << "               Bilangan = "; cin >> bil1;
+            if(oper == "+" | oper == "-" | oper == "x" | oper == "*" | oper == ":" | oper == "/" | oper == "^" | oper == "sin" | oper == "cos" | oper == "tan"){
+                if(oper == "+" | oper == "-" | oper == "x" | oper == "*" | oper == ":" | oper == "/" | oper == "^"){
+                    input1 :
+                        cout << "               Bilangan    = "; cin >> bil1;
 
-                rumus :
-                    if(oper == '+'){
-                        cout << "               Bilangan = "; cin >> bil2;
-                        hsl = bil1 + bil2;
-                    }
-                    else if(oper == '-'){
-                        cout << "               Bilangan = "; cin >> bil2;
-                        hsl = bil1 - bil2;              
-                    }
-                    else if(oper == 'x' | oper == '*'){
-                        cout << "               Bilangan = "; cin >> bil2;
-                        hsl = bil1 * bil2;              
-                    }
-                    else if(oper == ':'){
-                        cout << "               Bilangan = "; cin >> bil2;
-                        hsl = bil1 / bil2;          
-                    }
-                    else if(oper == '^'){
-                        cout << "               Pangkat  = "; cin >> bil2;
-                        hsl = pow(bil1, bil2);
-                    }
+                    rumus :
+                        if(oper == "+"){
+                            cout << "               Bilangan    = "; cin >> bil2;
+                            hsl = bil1 + bil2;
+                        }
+                        else if(oper == "-"){
+                            cout << "               Bilangan    = "; cin >> bil2;
+                            hsl = bil1 - bil2;              
+                        }
+                        else if(oper == "x" | oper == "*"){
+                            cout << "               Bilangan    = "; cin >> bil2;
+                            hsl = bil1 * bil2;              
+                        }
+                        else if(oper == ":" | oper == "/"){
+                            cout << "               Bilangan    = "; cin >> bil2;
+                            hsl = bil1 / bil2;          
+                        }
+                        else if(oper == "^"){
+                            cout << "               Pangkat     = "; cin >> bil2;
+                            hsl = pow(bil1, bil2);
+                        }
+                }
+                else if (oper == "sin" | oper == "cos" | oper == "tan"){
+                    cout << "               Bilangan    = "; cin >> bil2;
 
-                while(oper != '='){
-                    cout << "\n               Jenis Operasi ( + | - | x or * | : or / | ^ | = )? "; cin >> oper;
-
-                    if(oper == '+' | oper == '-' | oper == 'x' | oper == ':' | oper == '^'){
-                        cout << "               Hasil    = " << hsl << endl;
-
-                        bil1 = hsl;
-                        goto rumus;
-                    }
-                    else if(oper == '='){
-                        goto akhir;
-                    }
-                    else{
-                        cout << "               Operasi tidak tersedia." << endl;
-                    }
+                    trigonometri :
+                        if(oper == "sin"){
+                            hsl = sin(bil2);
+                            cout << "               Hasil       = " << hsl << endl;
+                        }
+                        else if(oper == "cos"){
+                            hsl = cos(bil2);
+                            cout << "               Hasil       = " << hsl << endl;
+                        }
+                        else if(oper == "tan"){
+                            hsl = tan(bil2);
+                            cout << "               Hasil       = " << hsl << endl;
+                        }
                 }
 
-                akhir:
-                    cout << "               Hasil Akhir = " << hsl << endl << endl;
+                if(isinf(hsl)){
+                    goto akhir;
+                }
+                else{
+                    while(oper != "="){
+                        cout << "\n               Jenis Operasi ( + | - | x or * | : or / | ^ | sin | cos | tan | = )? "; cin >> oper;
+
+                        if(oper == "+" | oper == "-" | oper == "x" | oper == "*" | oper == ":" | oper == "/" | oper == "^"){
+                            cout << "               Hasil       = " << hsl << endl;
+
+                            bil1 = hsl;
+                            goto rumus;
+                        }
+                        else if(oper == "="){
+                            goto akhir;
+                        }
+                        else if(oper == "sin" | oper == "cos" | oper == "tan"){
+                            bil2 = hsl;
+                            goto trigonometri;
+                        }
+                        else{
+                            cout << "               Operasi tidak tersedia." << endl;
+                        }
+                    }
+
+                    akhir:
+                        cout << "               Hasil Akhir = " << hsl << endl << endl;
+                }
             }
             else{
                 cout << "               Operasi tidak tersedia." << endl << endl;
 
                 goto operasi;
-                goto input;
+                goto input1;
             }
         }
 };
 
-int main() {
+int main(){
     Rumus rms;
 
     return 0;
