eb5b453207791dc276e3a4b0eb8a9abebeb63b41
davin056 <65593334+davin056@users.noreply.github.com>
2020-06-02 23:16:32 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,14 +7,20 @@ using namespace std;
 int main() {
 
 	ofstream os("test.txt", ios::app);
-	string nama;
+	string uname,rating;
 	map<string, string> data;
   	map<string,string>::iterator itr;
 
+	// pada bagian constructor
 	cout << "Masukkan nama pengguna: ";
-	getline(cin,nama); // input nama
-	os << "\n" << nama; // menambahkan nama ke txt
-	data.insert(pair<string, string>(nama,"pengguna")); // membuat map berisi nama pengguna dan "pengguna" untuk memenuhi map
+	getline(cin,uname); // input nama	
+	
+	//pada bagian destructor
+	cout << "Beri peringkat 1-10: ";
+	cin >> rating; //input rating
+	os << "\n" << uname; // menambahkan nama ke txt
+	os << "\n" << rating; // menambahkan rating ke txt
+	data.insert(pair<string, string>(uname,rating)); // membuat map berisi uname dan rating untuk memenuhi map
 	itr = data.begin(); // karena mapnya cuma satu jadi langsung disimpan di iterator
 	cout << itr->first; // output map
 
